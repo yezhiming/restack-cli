557734d92b17eee006d09a0f25c223bcbd914ebc
Justin Yip <justin.yip@foxmail.com>
2016-09-03 21:23:12 +0800

    dll script, fix output dir

--- src/webpack/webpack.dll.config.js
+++ src/webpack/webpack.dll.config.js
@@ -4,19 +4,19 @@ module.exports = {
   entry: {
     // create two library bundles, one with jQuery and
     // another with Angular and related libraries\
-    vendors: ['react', 'react-dom', 'react-router', 'history',
-      'react-addons-pure-render-mixin',"rc-input-number",
-      'lodash', 'react-redux', 'redux', 'react-router-redux',
-      'redux-thunk', 'redux-undo',  'react-bootstrap', 'jquery',
-      "rc-select", "rc-tree", "rc-tree-select", "rc-notification",
-      "moment","highlight.js","jquery-ui","react-toastr","classnames",
-      "fullcalendar", "simditor"
+    vendors: [
+      'react', 'react-dom', 'react-addons-pure-render-mixin', // react stuff
+      'redux', 'react-redux', 'redux-thunk', // redux stuff
+      'react-router', 'history', 'react-router-redux', // router stuff
+      'lodash', "classnames",
+      'jquery', "moment",
+      'react-bootstrap'
     ]
   },
 
   output: {
     filename: '[name].bundle.js',
-    path: './static-dist/',
+    path: './dll',
 
     // The name of the global variable which the library's
     // require() function will be assigned to
@@ -28,7 +28,7 @@ module.exports = {
       // The path to the manifest file which maps between
       // modules included in a bundle and the internal IDs
       // within that bundle
-      path: './static-dist/[name]-manifest.json',
+      path: './dll/[name]-manifest.json',
       // The name of the global variable which the library's
       // require function has been assigned to. This must match the
       // output.library option above
