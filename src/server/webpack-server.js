ae701825090822ea000dcfe91cb8772aecd289ac
Justin Yip <justin.yip@foxmail.com>
2016-11-18 17:51:01 +0800

    根据环境选择配置

--- src/server/webpack-server.js
+++ src/server/webpack-server.js
@@ -14,12 +14,13 @@ var app = new express();
 var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
-var config = require('../webpack/webpack.dev.config')(program.cwd, project);
+var config = require(`../webpack/webpack.${program.env}.config`)(program.cwd, project);
 
 // ref: http://webpack.github.io/docs/webpack-dev-server.html#combining-with-an-existing-server
 
 var compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, {
+  debug: true,
   noInfo: true,
   publicPath: "/",
   stats: { colors: true }
